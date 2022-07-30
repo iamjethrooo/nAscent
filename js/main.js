8e1752a1d50f8f8a046e60e7f8f09a3390a739af
Jethro <estangkij@gmail.com>
2022-07-30 21:03:40 +0800

    Add animations on scroll for services

--- js/main.js
+++ js/main.js
@@ -8,19 +8,41 @@ $(document).ready(function () {
       2000
     );
   });
-});
 
-const images = document.querySelectorAll(".service-image img");
-const modalwrap = document.querySelector(".modal-wrap");
-const modalcontent = document.querySelector(".modal-content");
+  // AOS
+  AOS.init({
+    easing: "ease",
+    duration: 1000,
+    once: true, /* Animation only runs once */
+  })
 
-images.forEach(image => {
-  image.addEventListener('click', function() {
-    modalcontent.style.backgroundImage = `url(${image.src})`;
-    modalwrap.style.display = 'flex';
+  document.querySelector('.wrapper').addEventListener('scroll', AOS.refresh);
+
+  // AOS for mobile screens
+  if (document.body.clientWidth < 992) {
+    const serviceImageDiv = document.querySelectorAll(".service-image");
+    const serviceDescriptionDiv = document.querySelectorAll(".service-description");
+    serviceImageDiv.forEach(div => {
+      div.setAttribute("data-aos", "slide-up");
+    });
+    serviceDescriptionDiv.forEach(div => {
+      div.setAttribute("data-aos", "slide-down");
+    })
+  }
+
+  const images = document.querySelectorAll(".service-image img");
+  const modalwrap = document.querySelector(".modal-wrap");
+  const modalcontent = document.querySelector(".modal-content");
+
+  images.forEach(image => {
+    image.addEventListener('click', function() {
+      modalcontent.style.backgroundImage = `url(${image.src})`;
+      modalwrap.style.display = 'flex';
+    });
+  });
+
+  modalwrap.addEventListener('click', function() {
+    modalwrap.style.display = 'none';
   });
 });
 
-modalwrap.addEventListener('click', function() {
-  modalwrap.style.display = 'none';
-});
